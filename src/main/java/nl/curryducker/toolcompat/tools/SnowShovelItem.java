29ac4fb783e2d50841c78711665eb286d7bce481
curryducker <66020230+curryducker@users.noreply.github.com>
2023-01-15 14:30:27 +0100

    Initial commit

---
+++ src/main/java/nl/curryducker/toolcompat/tools/SnowShovelItem.java
@@ -0,0 +1,31 @@
+package nl.curryducker.toolcompat.tools;
+
+import net.mcreator.frostedfriends.procedures.WoodenSnowShovelBlockDestroyedWithToolProcedure;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.List;
+
+public class SnowShovelItem extends ShovelItem {
+    public SnowShovelItem(Tier p_43114_, float p_43115_, float p_43116_, Properties p_43117_) {
+        super(p_43114_, p_43115_, p_43116_, p_43117_);
+    }
+
+    public boolean mineBlock(ItemStack itemstack, Level world, BlockState blockstate, BlockPos pos, LivingEntity entity) {
+        boolean retval = super.mineBlock(itemstack, world, blockstate, pos, entity);
+        WoodenSnowShovelBlockDestroyedWithToolProcedure.execute(world, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ());
+        return retval;
+    }
+
+    public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+        super.appendHoverText(itemstack, world, list, flag);
+        list.add(Component.literal("§8Remove snow in a larger area"));
+    }
+}
